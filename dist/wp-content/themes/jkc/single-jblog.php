3d63947f828615999dee80401c9829bd67474be6
naoki.k <80358990+apollo-halfx@users.noreply.github.com>
2025-03-29 23:33:45 +0900

    ✨ インデントやタブの体裁を修正

--- dist/wp-content/themes/jkc/single-jblog.php
+++ dist/wp-content/themes/jkc/single-jblog.php
@@ -1,28 +1,28 @@
 <?php get_header(); ?>
 
 <main>
- <div class="p-sub-fv l-sub-fv">
+  <div class="p-sub-fv l-sub-fv">
 
-  <div class="p-sub-fv__container l-container">
-   <div class="p-sub-fv__title c-page-title">
-    <h1 class="c-page-title__main"><?php the_title(); ?></h1>
-   </div>
+    <div class="p-sub-fv__container l-container">
+      <div class="p-sub-fv__title c-page-title">
+        <h1 class="c-page-title__main"><?php the_title(); ?></h1>
+      </div>
+    </div>
+
   </div>
 
- </div>
+  <div class="p-sub-fv__breadcrumbs c-breadcrumbs l-breadcrumbs">
+    <?php if (function_exists('bcn_display')) {
+      bcn_display();
+    }; ?>
+  </div>
 
- <div class="p-sub-fv__breadcrumbs c-breadcrumbs l-breadcrumbs">
-  <?php if (function_exists('bcn_display')) {
-   bcn_display();
-  }; ?>
- </div>
-
- <div class="l-container l-container--sub">
-  <p class="c-text u-text-align-right">
-   <?php echo esc_html(get_the_date('Y.m.d')); ?>
-  </p>
-  <?php the_content(); ?>
- </div>
+  <div class="l-container l-container--sub">
+    <p class="c-text u-text-align-right">
+      <?php echo esc_html(get_the_date('Y.m.d')); ?>
+    </p>
+    <?php the_content(); ?>
+  </div>
 
 </main>
 
