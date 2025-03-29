c50db584e6107a386ddbb2482739ca49f4d3cdbc
naoki.k <80358990+apollo-halfx@users.noreply.github.com>
2025-03-30 02:12:44 +0900

    ✨ ジャックブログに記事ナビゲーションを追加

--- dist/wp-content/themes/jkc/single-jblog.php
+++ dist/wp-content/themes/jkc/single-jblog.php
@@ -22,6 +22,37 @@
       <?php echo esc_html(get_the_date('Y.m.d')); ?>
     </p>
     <?php the_content(); ?>
+
+    <div class="p-jblog-navigation">
+      <div class="p-jblog-navigation__next-prev-link">
+        <div class="p-jblog-navigation__item">
+          <?php
+          $next_post = get_adjacent_post(false, '', false);
+          if (!empty($next_post)) :
+          ?>
+            <a class="p-jblog-navigation__link p-jblog-navigation__link--next" href="<?php echo esc_url(get_permalink($next_post->ID)); ?>">次の記事へ</a>
+          <?php endif; ?>
+        </div>
+        <div class="p-jblog-navigation__item">
+          <?php
+          $prev_post = get_adjacent_post(false, '', true);
+          if (!empty($prev_post)) :
+          ?>
+            <a class="p-jblog-navigation__link p-jblog-navigation__link--prev" href="<?php echo esc_url(get_permalink($prev_post->ID)); ?>">前の記事へ</a>
+          <?php endif; ?>
+        </div>
+      </div>
+      <div class="p-jblog-navigation__archive-link">
+        <?php
+        // カスタム投稿タイプのアーカイブページURLを取得
+        $archive_url = get_post_type_archive_link('jblog');
+        ?>
+        <div class="p-jblog-navigation__item">
+          <a href="<?php echo esc_url($archive_url); ?>" class="p-jblog-navigation__link p-jblog-navigation__link--archive">一覧へ戻る</a>
+        </div>
+      </div>
+    </div>
+
   </div>
 
 </main>
